52a0c4926a582296fcd464f7f6aeed2f612ea89a
Benjamin Franzke <bfr@qbus.de>
2016-11-22 06:15:45 +0100

    Add Hook that sends a X-TYPO3-Cache status header

---
+++ Classes/Hooks/DebugTypo3CacheHit.php
@@ -0,0 +1,23 @@
+<?php
+namespace Qbus\CacheStatus\Hooks;
+
+/**
+ * DebugTypo3CacheHit
+ *
+ * @author Benjamin Franzke <bfr@qbus.de>
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class DebugTypo3CacheHit
+{
+    /**
+     * @return void
+     */
+    public function debug($params, $pObj)
+    {
+        if ($pObj->cacheContentFlag) {
+            header('X-TYPO3-Cache: Hit');
+        } else {
+            header('X-TYPO3-Cache: Miss');
+        }
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,6 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][$_EXTKEY] =
+    \Qbus\CacheStatus\Hooks\DebugTypo3CacheHit::class . '->debug';
