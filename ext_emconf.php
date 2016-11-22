87635f7c81a43e98803b653c024786b406342027
Benjamin Franzke <bfr@qbus.de>
2016-11-22 06:13:48 +0100

    Initial commit
    ..as empty TYPO3 extension

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'cache_status',
+    'description' => '',
+    'category' => '',
+    'author' => '',
+    'author_email' => 'bfr@qbus.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Qbus\\CacheStatus\\' => 'Classes',
+        ),
+    ),
+);
