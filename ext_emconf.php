5cd964ec6f13eef6735a765edbf9723af3682048
Benjamin Franzke <bfr@qbus.de>
2017-12-19 11:06:58 +0100

    Mark as compatible with TYPO3 v9.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.2.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.0.99',
         ),
         'conflicts' => array(
         ),
