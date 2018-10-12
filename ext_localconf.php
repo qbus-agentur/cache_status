09b49df440b39a75aa3ba970a1149c330f60826e
Benjamin Franzke <bfr@qbus.de>
2018-10-12 14:09:29 +0200

    Be compatible with TYPO3 v9.5

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][$_EXTKEY] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['cache_status'] =
     \Qbus\CacheStatus\Hooks\DebugTypo3CacheHit::class . '->debug';
